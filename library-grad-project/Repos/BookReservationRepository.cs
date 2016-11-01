b5be871d550d0bc60077f3d5711b5f8e70569d8d
Georgiana <gginghina@scottlogic.com>
2016-11-01 16:33:19 +0000

    added book reservation repo

--- library-grad-project/Repos/BookReservationRepository.cs
+++ library-grad-project/Repos/BookReservationRepository.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using LibraryGradProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace LibraryGradProject.Repos
 {
-    public class BookReservationRepository
+    public class BookReservationRepository : IRepository<BookReservation>
     {
+        private List<BookReservation> _bookResCollection = new List<BookReservation>(); 
+
+        public void Add(BookReservation entity)
+        {
+            entity.Id = _bookResCollection.Count;
+            _bookResCollection.Add(entity);
+        }
+
+        public BookReservation Get(int id)
+        {
+            return _bookResCollection.Where(bookRes => bookRes.Id == id).SingleOrDefault();
+        }
+
+        public IEnumerable<BookReservation> GetAll()
+        {
+            return _bookResCollection;
+        }
+
+        public void Remove(int id)
+        {
+            BookReservation bookResToRemove = Get(id);
+            _bookResCollection.Remove(bookResToRemove);
+        }
     }
 }
