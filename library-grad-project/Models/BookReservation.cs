65f77188adab3171615b34a3b94f3969e75c27f0
Georgiana <gginghina@scottlogic.com>
2016-11-01 16:04:39 +0000

    added book reservation model

---
+++ library-grad-project/Models/BookReservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryGradProject.Models
+{
+    public class BookReservation
+    {
+        public Book book;
+        public DateTime timestamp;
+
+        public BookReservation(Book bookToReserve)
+        {
+            book = bookToReserve;
+            DateTime timestamp = DateTime.UtcNow;
+        }
+    } 
+}
