a0abcdd6ffbd89eed0b00568c626398a9df20936
Georgiana <gginghina@scottlogic.com>
2016-11-02 16:58:35 +0000

    added tables

---
+++ library-grad-project/App_Data/BookReservations.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Book_Id] INT NOT NULL, 
+    [from] DATETIME NOT NULL, 
+    [to] DATETIME NOT NULL
+)
---
+++ library-grad-project/App_Data/Books.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ISBN] NVARCHAR(50) NULL, 
+    [Title] NVARCHAR(50) NULL, 
+    [Author] NVARCHAR(50) NULL, 
+    [PublishDate] DATE NULL
+)
