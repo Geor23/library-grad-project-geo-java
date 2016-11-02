5002aa3c84206be400fc3cd7e4c5540836897430
Georgiana <gginghina@scottlogic.com>
2016-11-02 10:24:37 +0000

    cleaned up

--- library-grad-project/Controllers/BookReservationsController.cs
+++ library-grad-project/Controllers/BookReservationsController.cs
@@ -1,9 +1,6 @@
 ﻿using LibraryGradProject.Models;
 using LibraryGradProject.Repos;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
 
 namespace LibraryGradProject.Controllers
