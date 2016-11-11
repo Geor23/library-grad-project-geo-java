157dfabb292ac888e0908cac9fbbda531cf96822
Georgiana <gginghina@scottlogic.com>
2016-11-11 11:34:39 +0000

    connected database

---
+++ library-grad-project/Context/BookContext.cs
@@ -0,0 +1,11 @@
+﻿using LibraryGradProject.Models;
+using System.Data.Entity;
+
+namespace LibraryGradProject.Context
+{
+    public class BookContext : DbContext
+    {
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<BookReservation> BookReservations { get; set; }
+    }
+}
