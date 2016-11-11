bb14e1fac56375b1b25b9423daf6559b8e7bba53
Georgiana <gginghina@scottlogic.com>
2016-11-11 11:56:17 +0000

    changed book repo to use context and db

---
+++ library-grad-project/Repos/BookDbRepository.cs
@@ -0,0 +1,59 @@
+﻿using LibraryGradProject.Context;
+using LibraryGradProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace LibraryGradProject.Repos
+{
+    public class BookDbRepository : IRepository<Book>
+    {
+        public BookDbRepository() : base()
+        {
+        }
+
+        public BookDbRepository(BookContext ctx) : base(ctx)
+        {
+        }
+
+        public override void Add(Book entity)
+        {
+            using (BookContext context = GetContext())
+            {
+                context.Attach(entity);
+                context.Books.Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public override IEnumerable<Book> GetAll()
+        {
+            using (BookContext context = GetContext())
+            {
+                return context.Books.ToList();
+            }
+        }
+
+        public override Book Get(int id)
+        {
+            using (BookContext context = GetContext())
+            {
+                return context.Books.Where(book => book.Id == id).SingleOrDefault();
+            }
+        }
+
+        public override void Remove(int id)
+        {
+            using (BookContext context = GetContext())
+            {
+                Book book = Get(id);
+                if (book != null)
+                {
+                    context.Attach(book);
+                    context.Books.Remove(book);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
