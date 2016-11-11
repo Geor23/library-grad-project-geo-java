2a0df3bf2c9dd367f3ca38454e3ea7ccd7e757ab
Georgiana <gginghina@scottlogic.com>
2016-11-11 11:55:21 +0000

    changed repo to use context

--- library-grad-project/Repos/IRepository.cs
+++ library-grad-project/Repos/IRepository.cs
@@ -1,12 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using LibraryGradProject.Context;
+using System.Collections.Generic;
 
 namespace LibraryGradProject.Repos
 {
-    public interface IRepository<T>
+    public abstract class IRepository<T>
     {
-        T Get(int id);
-        IEnumerable<T> GetAll();
-        void Add(T entity);
-        void Remove(int id);
+        private BookContext context;
+
+        public IRepository()
+        {
+        }
+
+        public IRepository(BookContext ctx)
+        {
+            context = ctx;
+        }
+
+        public BookContext GetContext()
+        {
+            if (context == null)
+            {
+                return new BookContext();
+            } else
+            {
+                return context;
+            }
+        }
+
+        public abstract T Get(int id);
+        public abstract IEnumerable<T> GetAll();
+        public abstract void Add(T entity);
+        public abstract void Remove(int id);
     }
 }
