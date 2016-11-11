1fac8565b7f6f84b232651ac9d7fa0941f88db8d
Georgiana <gginghina@scottlogic.com>
2016-11-11 11:33:59 +0000

    deleted unnecessary sql script for creating tables

--- library-grad-project/App_Data/BookReservations.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Book_Id] INT NOT NULL, 
-    [from] DATETIME NOT NULL, 
-    [to] DATETIME NOT NULL
-)
--- library-grad-project/App_Data/Books.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [ISBN] NVARCHAR(50) NULL, 
-    [Title] NVARCHAR(50) NULL, 
-    [Author] NVARCHAR(50) NULL, 
-    [PublishDate] DATE NULL
-)
