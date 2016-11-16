249ce7146c7db9c8c22eb95b5ae49584e59078c7
Georgiana <gginghina@scottlogic.com>
2016-11-16 14:29:11 +0000

    added react setup files

---
+++ library-grad-project/wwwroot/js/app.jsx
@@ -0,0 +1,14 @@
+﻿var LibraryBox = React.createClass({
+    render: function () {
+        return (
+            <div className="LibraryBox">
+                Hi you!
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <LibraryBox />,
+    document.getElementById('content')
+);
