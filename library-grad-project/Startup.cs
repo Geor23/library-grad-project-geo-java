0b8fd9638ff16fe04b73f72757250ea4b05f12ac
Georgiana <gginghina@scottlogic.com>
2016-11-16 16:03:44 +0000

    fixed not loading start page index problem

--- library-grad-project/Startup.cs
+++ library-grad-project/Startup.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
+using Microsoft.AspNetCore.Http;
+using React.AspNet;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 [assembly: OwinStartup(typeof(LibraryGradProject.Startup))]
 
@@ -11,5 +19,9 @@ namespace LibraryGradProject
     public partial class Startup
     {
         public void Configuration(IAppBuilder app){ }
+
     }
+
+   
+
 }
