a3d14f6f5e75d83b2fa2b49f86c0da4d966fd4e7
Georgiana <gginghina@scottlogic.com>
2016-11-22 10:25:39 +0000

    added routing element

--- client/views/components/Container.jsx
+++ client/views/components/Container.jsx
@@ -1,11 +1,19 @@
 var React = require('react');
+var ReactRouter = require('react-router');
+var { Router, Route, IndexRoute, Link } = ReactRouter
+var browserHistory = ReactRouter.browserHistory;
 var Books = require('./Books.jsx');
+var BookForm = require('./BookForm.jsx');
 
 var Container = React.createClass({
 	render: function() {
 		return (
 			<div className="container">
-				<Books></Books>
+				<Router history={browserHistory}>
+			    	<Route path="/" component={Books} />
+			      	<Route path="/search" component={Books} />
+			      	<Route path="/add" component={BookForm} />
+			  	</Router>
 			</div>
 		);
 	}
