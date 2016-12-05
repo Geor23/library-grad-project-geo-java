19e6b4ca0d938df8b363d971cc2908c282a07bdb
Georgiana <gginghina@scottlogic.com>
2016-12-05 11:16:54 +0000

    added basic java spring project

---
+++ LibraryApp/src/main/java/com/scottlogic/LibraryAppApplication.java
@@ -0,0 +1,12 @@
+package com.scottlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppApplication.class, args);
+	}
+}
