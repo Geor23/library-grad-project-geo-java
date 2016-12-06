fad59c9c9a78f224aae3a37fad87485147c7635d
Georgiana <gginghina@scottlogic.com>
2016-12-06 09:45:50 +0000

    added start application code

--- LibraryApp/src/main/java/com/scottlogic/libraryapp/LibraryApp.java
+++ LibraryApp/src/main/java/com/scottlogic/libraryapp/LibraryApp.java
@@ -1,12 +1,12 @@
-package com.scottlogic;
+package com.scottlogic.libraryapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryAppApplication {
+public class LibraryApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryAppApplication.class, args);
+	public static void main(String args[]) {
+		SpringApplication.run(LibraryApp.class, args);
 	}
 }
