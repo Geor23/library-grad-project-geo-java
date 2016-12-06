d52c4fbd5d2c1d24ebcc74909138d63b5a7c6a28
Georgiana <gginghina@scottlogic.com>
2016-12-06 09:47:46 +0000

    added book repository code

---
+++ LibraryApp/src/main/java/com/scottlogic/libraryapp/dao/BookRepository.java
@@ -0,0 +1,13 @@
+package com.scottlogic.libraryapp.dao;
+
+import com.scottlogic.libraryapp.domain.Book;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+	Optional<Book> findByTitleIgnoringCase(final String title);
+	Optional<Book> findByAuthorIgnoringCase(final String author);
+	Optional<Book> findByIsbnIgnoringCase(final String isbn);
+}
