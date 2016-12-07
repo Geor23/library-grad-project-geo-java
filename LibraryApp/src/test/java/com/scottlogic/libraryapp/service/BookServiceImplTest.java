23e57092a3bc3746c03a5d9ae8f1bf151d485738
Georgiana <gginghina@scottlogic.com>
2016-12-07 16:34:34 +0000

    added book service tests

---
+++ LibraryApp/src/test/java/com/scottlogic/libraryapp/service/BookServiceImplTest.java
@@ -0,0 +1,96 @@
+package com.scottlogic.libraryapp.service;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.scottlogic.libraryapp.dao.BookRepository;
+import com.scottlogic.libraryapp.domain.Book;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BookServiceImplTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    private BookService bookService;
+
+    @Before
+    public void setUp(){
+        bookService = new BookServiceImpl(bookRepository);
+    }
+    
+    @Test
+    public void shouldAddBook() {
+    	final Book book = new Book("Title", "Author", "Isbn");
+    	
+    	bookService.add(Optional.of(book));
+    	verify(bookRepository).save(book);
+    } 
+    
+    @Test
+    public void shouldEditBook() {
+    	final Book book = new Book("Title", "Author", "Isbn");
+    	
+    	bookService.editBook(Optional.of(book));
+    	verify(bookRepository).save(book);
+    }
+    
+    @Test
+    public void shouldDeleteBook() {
+    	final Book book = new Book("Title", "Author", "Isbn");
+    	
+    	bookService.deleteBook(Optional.of(book));
+    	verify(bookRepository).delete(book);
+    }
+    
+    @Test
+    public void shouldGetBookByTitle() {
+    	when(bookRepository.findByTitleIgnoringCase("Title")).thenReturn(Optional.of(new Book("Title", "Author", "Isbn")));
+    	
+    	final Optional<Book> result = bookService.getByTitle("Title");
+    	assertThat(result.get().getTitle(), equalTo("Title"));
+    }
+    
+    @Test
+    public void shouldGetBookByAuthor() {
+    	when(bookRepository.findByAuthorIgnoringCase("Author")).thenReturn(Optional.of(new Book("Title", "Author", "Isbn")));
+    	
+    	final Optional<Book> result = bookService.getByAuthor("Author");
+    	assertThat(result.get().getAuthor(), equalTo("Author"));
+    }
+    
+    @Test
+    public void shouldGetBookByIsbn() {
+    	when(bookRepository.findByIsbnIgnoringCase("Isbn")).thenReturn(Optional.of(new Book("Title", "Author", "Isbn")));
+    	
+    	final Optional<Book> result = bookService.getByIsbn("Isbn");
+    	assertThat(result.get().getIsbn(), equalTo("Isbn"));
+    }
+    
+    @Test
+    public void shouldGetBookById() {
+    	when(bookRepository.findOne(1)).thenReturn(new Book("Title", "Author", "Isbn"));
+    	
+    	final Optional<Book> result = bookService.getById(1);
+    	assertThat(result.get().getIsbn(), equalTo("Isbn"));
+    }
+    
+    @Test
+    public void shouldGetAll() {
+    	when(bookRepository.findAll()).thenReturn(Arrays.asList(new Book("Title", "Author", "Isbn")));
+    	assertThat(bookService.getAll(), equalTo(Arrays.asList(new Book("Title", "Author", "Isbn"))));
+    }
+
+}
